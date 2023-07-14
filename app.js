6284f0bdeba99fb911d3aba27dbf7babf50d49ff
anns318 <an310898@gmai.com>
2023-07-14 18:44:49 +0700

    add template engine with handlerBar

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const express = require("express");
 const path = require("path");
 const rootDir = require("./util/path");
+const expressHbs = require("express-handlebars");
 
-const adminRoute = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoute = require("./routes/shop");
 
 const app = express();
+
+app.engine(
+	"hbs",
+	expressHbs.engine({
+		extname: ".hbs",
+		layoutsDir: "views/layouts",
+		defaultLayout: "main",
+	})
+);
+app.set("view engine", "hbs");
+app.set("views", "views");
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
 app.use(shopRoute);
-app.use("/admin", adminRoute);
+app.use("/admin", adminData.routes);
 
 app.use("/", (req, res, next) => {
-	res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+	res.render("404", { pageTitle: "Page not found" });
 });
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -3,8 +3,16 @@ const path = require("path");
 const rootDir = require("../util/path");
 const router = express.Router();
 
+const adminData = require("./admin");
+
 router.get("/", (req, res, next) => {
-	res.sendFile(path.join(rootDir, "views", "shop.html"));
+	res.render("shop", {
+		products: adminData.products,
+		show: adminData.products.length > 0,
+		productCSS: true,
+		pageTitle: "My shop",
+		activeShop: true,
+	});
 });
 
 module.exports = router;
