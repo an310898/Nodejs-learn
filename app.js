54df730214dd26853904bc7aa71ddfa42833f098
anns318 <an310898@gmai.com>
2023-07-14 12:15:50 +0700

    filtering path

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 
 app.use(shopRoute);
-app.use(adminRoute);
+app.use("/admin", adminRoute);
 
 app.use("/", (req, res, next) => {
 	res.status(404).send("<h1>404</h1>");
