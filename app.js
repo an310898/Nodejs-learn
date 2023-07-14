4e3ac379daf4c92cf550f47c75774f503350522a
anns318 <an310898@gmai.com>
2023-07-14 12:04:38 +0700

    add 404 return

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 
-app.use(adminRoute);
 app.use(shopRoute);
+app.use(adminRoute);
 
+app.use("/", (req, res, next) => {
+	res.status(404).send("<h1>404</h1>");
+});
 app.listen(3000);
