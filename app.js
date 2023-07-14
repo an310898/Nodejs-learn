f38f0bf619539298eace9e44fc8c3a3bb0eb4572
anns318 <an310898@gmai.com>
2023-07-14 10:49:36 +0700

    Middleware excution to POST request

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/", (req, res, next) => {
 	next();
 });
-app.use("/add-product", (req, res, next) => {
+app.get("/add-product", (req, res, next) => {
 	res.send(
 		`<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add</button></form>`
 	);
 });
-app.use("/product", (req, res, next) => {
+app.post("/product", (req, res, next) => {
 	console.log(req.body);
 	res.redirect("/");
 });
