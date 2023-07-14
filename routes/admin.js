58972e44e60e04a33f702db7162403f1bf45e23f
anns318 <an310898@gmai.com>
2023-07-14 13:19:46 +0700

    serving file statically with css file

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,11 @@
 const express = require("express");
+const path = require("path");
+const rootDir = require("../util/path");
+
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-	res.send("<h1>Admin</h1>");
-});
-
 router.get("/add-product", (req, res, next) => {
-	res.send(
-		`<form action="/admin/add-product" method="POST"><input type="text" name="title" /><button type="submit">Add</button></form>`
-	);
+	res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 router.post("/add-product", (req, res, next) => {
 	console.log(req.body);
