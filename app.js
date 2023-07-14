c077fc7eba8bcae6cf8548f388eb05f8d227d5dd
anns318 <an310898@gmai.com>
2023-07-14 10:46:42 +0700

    bodyParser for read res.body

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require("express");
-
+// const bodyParser = require("body-parser");
 const app = express();
-app.use((req, res, next) => {
-	console.log("in the middleware");
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use("/", (req, res, next) => {
 	next();
 });
-app.use((req, res, next) => {
-	console.log("in another middleware");
+app.use("/add-product", (req, res, next) => {
+	res.send(
+		`<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add</button></form>`
+	);
+});
+app.use("/product", (req, res, next) => {
+	console.log(req.body);
+	res.redirect("/");
+});
+app.use("/", (req, res, next) => {
 	res.send("<h1>Hello Express</h1>");
 });
-
 app.listen(3000);
