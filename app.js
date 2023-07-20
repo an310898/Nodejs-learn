24f36a3a9828fbeb22876d98d10f1f11cb7656bd
anns318 <an310898@gmai.com>
2023-07-21 01:38:19 +0700

    fake loged in user to keep userId request

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const expressHbs = require("express-handlebars");
 
 const adminRoute = require("./routes/admin");
 const shopRoute = require("./routes/shop");
+const userModel = require("./models/user");
 const fileNotFoundController = require("./controllers/404");
-const { run, mongoClient } = require("./util/database");
+const { mongoClient } = require("./util/database");
 
 const app = express();
 
@@ -22,9 +23,19 @@ app.set("views", "views");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use((req, res, next) => {
+  userModel
+    .findById("64b8f415a2d500e4f9b73050")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 app.use(shopRoute);
 app.use("/admin", adminRoute);
 
 app.use("/", fileNotFoundController.get404);
-mongoClient(() => app.listen(3000));
+mongoClient(() => {
+  app.listen(3000);
+});
